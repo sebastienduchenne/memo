ed132ed5c0d7e4154ed80b596d86b83a1c11c5b2
seb <duchenne.sebastien@gmail.com>
2019-06-10 23:28:19 +0200

    5

--- projets/docker-nodejs/controller.js
+++ projets/docker-nodejs/controller.js
@@ -1,24 +1,38 @@
-exports.create = (req, res) => {
+var fs = require("fs");
 
-};
+let events;
+fs.readFile( "./events.json", 'utf8', function (err, data) {
+    events = data;
+});
 
 exports.list = (req, res) => {
-console.log("exports.list")
-
-    res.status(200).send("toto");
+    console.log("exports.list")
+    res.status(200).send(events);
+    //res.status(200).send("events");
 };
 
 exports.getById = (req, res) => {
-console.log("exports.getById")
+    console.log("exports.getById")
 
+    //console.log(events)
+
+    res.status(200).send("getById");
 };
 
-exports.patchById = (req, res) => {
-console.log("exports.patchById")
+exports.create = (req, res) => {
+    console.log("exports.create")
 
+    res.status(200).send("create");
+};
+
+exports.updateById = (req, res) => {
+    console.log("exports.updateById")
+
+    res.status(200).send("update");
 };
 
 exports.removeById = (req, res) => {
-console.log("exports.removeById")
+    console.log("exports.removeById")
 
+    res.status(200).send("remove");
 };
