f6ead3a1b8272bb01be053d1913e59c90375573d
Sebastien Duchenne <sduchenne@sduchenne.vm.vdi.s1.p.fti.net>
2020-04-17 18:10:19 +0200

    17 avril 2020 nodejs 1

--- ECMAScript/nodejs/module.js
+++ ECMAScript/nodejs/module.js
@@ -3,6 +3,19 @@
 -2 standarts pour écrire des modules
     CommonJS, implémentation : Node.js
     AMD (Asynchronous Module Definition), implémentation : requireJS
+
+
+module : variable qui représente le module
+exports : objet
+
+Message.js
+module.exports = 'Hello world';
+
+app.js
+const msg = require(message.js)
+console.log(msg) // Hello world
+
+
 */
 
 //Parameters.js
