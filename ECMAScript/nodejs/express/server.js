5290a0f372036e8c0e238cdce3d0afc32b4bc959
seb <duchenne.sebastien@gmail.com>
2020-04-18 21:57:15 +0200

    18 avril 2020 nodejs

---
+++ ECMAScript/nodejs/express/server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hi!')
+})
+
+const server = app.listen(3000, () => console.log('Server ready'))
