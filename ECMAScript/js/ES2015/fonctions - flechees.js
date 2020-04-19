efefb01d3f0c52d5c75dd5c10c33ad9c5680084c
seb <duchenne.sebastien@gmail.com>
2020-04-19 23:41:55 +0200

    19 avril 2020 js

--- ECMAScript/js/ES2015/fonctions - flechees.js
+++ ECMAScript/js/ES2015/fonctions - flechees.js
@@ -1,8 +1,6 @@
 /*
 Une expression de fonction fléchée (arrow function en anglais) permet d’avoir une syntaxe plus courte que les expressions de fonction et ne possède pas ses propres valeurs pour this, arguments, super, ou new.target. Les fonctions fléchées sont souvent anonymes et ne sont pas destinées à être utilisées pour déclarer des méthodes.
 
-https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Fonctions/Fonctions_fl%C3%A9ch%C3%A9es
-
 */
 
 var a = [
