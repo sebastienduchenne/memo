0ba658bab6ded2b58ce34fc44167423f0a56b26b
seb <duchenne.sebastien@gmail.com>
2020-04-25 23:36:47 +0200

    25 avril 2020 js 2

--- ECMAScript/js/instructions/var - let - const.js
+++ ECMAScript/js/instructions/var - let - const.js
@@ -1,8 +1,15 @@
-//types var, let, const
-let x = 1; // déclarer une variable dont la portée est celle du bloc courant, globale en dehors de tout fonction
-const c = 0; // déclarer une constante dont la portée est celle du bloc courant, comme let, globale en dehors de tout fonction. Const est un abus de langage car c'est la référence vers la valeur qui ne peut pas être changé et non la valeur, donc on peut ajouter un élément à un tableau const ou modifier une propriété d'un objet const
-var v = 2; // déclarer une variable dont la portée est la fonction qui contient la déclaration, qu’importe le bloc dans lequel elle se trouve, et global en dehors de toute fonction
-//types primitifs : number, string, boolean
+/*
+-La portée lexicale d'une variable est la partie d'un programme où la liaison entre son identifiant et sa donnée est valide
+-la redéclaration dans une fonction fille d’une variable la masque dans la fonction fille, ne peut se faire qu’avec var
+-le changement d'une valeur change non pas la value mais change le pointage vers une nouvelle valeur
+-les variables déclarées avec var sont hoisted to the top
+-les variables sont passées par valeur, et les objets par référence
+-variable sans affectation est “undefined”
+*/
+
+let x = 1; // la portée est celle du bloc courant, globale en dehors de toute fonction
+const c = 0; // la portée est celle du bloc courant, comme let, globale en dehors de tout fonction. Const est un abus de langage car c'est la référence vers la valeur qui ne peut pas être changé et non la valeur, donc on peut ajouter un élément à un tableau const ou modifier une propriété d'un objet const
+var v = 2; // la portée est la fonction qui contient la déclaration, qu’importe le bloc dans lequel elle se trouve, et le contexte global en dehors de toute fonction
 
 //var et let : on peut aussi ne pas définir de valeur initiale, la variable aura donc une valeur undefined.
 //La déclaration sans instruction var (exemple : maVar = ‘test’) revient à écrire var maVar = ‘test’ dans un contexte global. La variable devient donc une propriété de l’objet global window
@@ -22,4 +29,25 @@ var x = 10;// Here x is 10
 // Here x is 10
 
 //--------------------------
+
+
+
+// 1. Avec initialiseur
+
+alert(maVariable1); // erreur : accès impossible avant l'initialisation
+alert(maVariable2); // erreur : accès impossible avant l'initialisation
+let maVariable1 = 5;
+const maVariable2 = 8;
+alert(maVariable1); // affiche 5
+alert(maVariable2); // affiche 8
+
+// 2. Sans initialiseur
+
+alert(maVariable3); // erreur : accès impossible avant l'initialisation
+alert(maVariable4); // erreur : accès impossible avant l'initialisation
+let maVariable3;
+const maVariable4;  // erreur : initialisation manquante
+alert(maVariable3); // affiche undefined
+alert(maVariable4); // erreur : initialisation manquante
+
 
--- ECMAScript/js/objets globaux/fonctions - promise.js
+++ ECMAScript/js/objets globaux/fonctions - promise.js
@@ -72,66 +72,6 @@ Promise.all([promise1, promise2, promise3]).then(function(values) {
 // expected output: Array [3, 42, "foo"]
 
 
-/////////////////// ASYNC AWAIT ///////////////////////////////////////
-
-/*
-- await = suspendre l’exécution jusqu’à ce que la promise soit réglée, utilisable dans les fonctions marquées de async. Si, dans une fonction async A, on appelle une fonction B avec await et que B n’est pas une promesse, alors B est convertie en promesse
-- async : une fonction définie avec le mot clé async renvoie systématiquement une promesse : si une erreur est levée pendant l’exécution de la fonction, la promesse est rejetée, et si une valeur est retournée, la promesse est résolue avec cette valeur.
-
-*/
-
-function getOTC() {
-  return new Promise(async (resolve) => {
-    //sans async await
-    axios.get(urls.otc[parameters.env]).then((response) => {
-      const ch = cheerio.load(response.data);
-      const otc = ch('#code-1').text();
-      resolve(otc);
-    })
-    .catch((e) => {
-      console.log(e)
-    });
-
-    //avec async await
-    const val = await axios.get(urls.otc[parameters.env]);
-    const ch = cheerio.load(val.data);
-    const otc = ch('#code-1').text();
-    resolve(otc);
-  });
-}
-
-//sans async-await
-it('should enter OTC', () => {
-  const promiseOTC = new Promise(resolve => resolve(lost.getOTC())); //il faut créer une promise
-  promiseOTC.then(
-    otc => console.log(otc),
-    error => console.log(error)
-  );
-})
-
-//avec async-await
-it('should enter OTC', async () => { //async
-  try {
-    const OTC = await lost.getOTC(); //OTC est une promesse, et on s'arrête qu'elle n'a pas été remplie
-  } catch (error){
-    console.log(error)
-  } finally{
-    //...
-  }
-
-  lost.enterOTC(OTC);
-})
-
-
-async function getNombreAsynchrone1() {/* traitement asynchrone (appel d’une API HTTP) */}
-async function getNombreAsynchrone2() {/* traitement asynchrone (appel d’une API HTTP) */}
-
-async function getAdditionAsynchrone() {
-  const nombre1 = await getNombreAsynchrone1();
-  const nombre2 = await getNombreAsynchrone2();
-  return nombre1 + nombre2;
-}
-
 
 /*
 https://javascript.info/promise-basics
