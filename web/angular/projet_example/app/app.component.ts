099b8c27eb86c6975274382ac0fd80fe00cd4935
seb <duchenne.sebastien@gmail.com>
2020-05-02 23:36:46 +0200

    2 mai 2020

--- web/angular/projet_example/app/app.component.ts
+++ web/angular/projet_example/app/app.component.ts
@@ -3,9 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root', // selector = l'endroit où inclure le composant dans src/index.html
   templateUrl: './app.component.html', // fichier html
+  // on peut juste mettre le html dans un string : `<h3>{{title}}<h3>`
   styleUrls: ['./app.component.css'] // fichier CSS
 })
 export class AppComponent {
   title = 'sandbox';
+  
   constructor() { }
 }
