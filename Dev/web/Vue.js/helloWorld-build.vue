ec32e06049059555a1a471291deb0eaab035ac30
sebastienduchenne <duchenne.sebastien@gmail.com>
2020-09-24 18:13:09 +0200

    24 sept 20

---
+++ Dev/web/Vue.js/helloWorld-build.vue
@@ -0,0 +1,21 @@
+<!-- à utiliser avec un outil de build tels que webpack ou Browserify -->
+
+<template>
+    <p>{{ msg }}</p>
+</template>
+
+<script>
+    module.exports = {
+        data: function () {
+            return {
+                msg: "Hello world"
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    p {
+        color: blue;
+    }
+</style>
