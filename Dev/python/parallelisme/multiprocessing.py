fb918f696b99dab85bc45337ff6d49b84206af5e
duchenne <duchenne.sebastien@gmail.com>
2020-11-09 23:24:28 +0100

    9 nov 2020

--- Dev/python/parallelisme/multiprocessing.py
+++ Dev/python/parallelisme/multiprocessing.py
@@ -51,3 +51,28 @@ if __name__ == '__main__':
 
     for num in range(10):
         Process(target=f, args=(lock, num)).start()
+
+
+# pool de workers : avec Pool
+'''
+-apply() : Appelle func
+-apply_async() : comme apply() mais renvoit un objet AsyncResult
+-close() : Empêche de nouvelles tâches d'être envoyées à la pool
+-terminate() : Stoppe immédiatement les processus workers sans finaliser les travaux courants
+-join() : Attend que les processus workers se terminent
+
+classe Pool.AsyncResult : renvoyée par apply_async() et map_async()
+    get() : Renvoie le résultat quand il arrive
+    wait() : Attend que le résultat soit disponible
+    ready() : Renvoie True ou False suivant si la tâche est accomplie
+    successful() : Renvoie True ou False suivant si la tâche est accomplie
+'''
+from multiprocessing import Pool
+
+def f(x):
+    return x*x
+
+if __name__ == '__main__':
+    pool = Pool(processes=4)            # démarrer un pool de 4 process
+    res = pool.apply_async(f, (20,))    # run 1 process
+    res_p = res.get(timeout=1)          # renvoit le résultat
