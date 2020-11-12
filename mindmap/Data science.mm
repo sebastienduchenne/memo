7de779282d8ed342ee2eca8706f0a08260305be8
duchenne <duchenne.sebastien@gmail.com>
2020-11-12 23:45:50 +0100

    12 nov 2020

---
+++ mindmap/Data science.mm
@@ -0,0 +1,114 @@
+<map version="freeplane 1.7.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Data science" FOLDED="false" ID="ID_1974547634" CREATED="1605218210592" MODIFIED="1605218221542" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="6" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Techniques" POSITION="right" ID="ID_921848012" CREATED="1605218227443" MODIFIED="1605218233688">
+<edge COLOR="#ff0000"/>
+<node TEXT="r&#xe9;gression lin&#xe9;aire" ID="ID_1375325143" CREATED="1605218239428" MODIFIED="1605218264291"/>
+<node TEXT="r&#xe9;gression logistique" ID="ID_1627669663" CREATED="1605218264801" MODIFIED="1605218279367"/>
+<node TEXT="Clustering" ID="ID_982158727" CREATED="1605218280423" MODIFIED="1605218307891"/>
+<node TEXT="Dimensionality reduction" ID="ID_1912231402" CREATED="1605218313056" MODIFIED="1605218354802"/>
+<node TEXT="Machine Learning" ID="ID_267689478" CREATED="1605218355666" MODIFIED="1605218369026"/>
+</node>
+<node TEXT="Langage" POSITION="right" ID="ID_1686205477" CREATED="1605218372128" MODIFIED="1605218376196">
+<edge COLOR="#0000ff"/>
+<node TEXT="Python" ID="ID_1047788203" CREATED="1605218378920" MODIFIED="1605218381932"/>
+<node TEXT="R" ID="ID_1296953263" CREATED="1605218382391" MODIFIED="1605218383392"/>
+<node TEXT="Julia" ID="ID_744171436" CREATED="1605218383807" MODIFIED="1605218386780"/>
+</node>
+<node TEXT="Frameworks" POSITION="right" ID="ID_38417626" CREATED="1605218388691" MODIFIED="1605218393387">
+<edge COLOR="#00ff00"/>
+<node TEXT="TensorFlow" ID="ID_1746237762" CREATED="1605218395399" MODIFIED="1605218406035"/>
+<node TEXT="Pytorch" ID="ID_492412165" CREATED="1605218406549" MODIFIED="1605218411505"/>
+<node TEXT="Jupyter Notebook" ID="ID_1991270254" CREATED="1605218412212" MODIFIED="1605218426499"/>
+<node TEXT="Apache Hadoop" ID="ID_928040032" CREATED="1605218427504" MODIFIED="1605218432588"/>
+</node>
+<node TEXT="Plateformes" POSITION="left" ID="ID_1403949355" CREATED="1605218490210" MODIFIED="1605218499930">
+<edge COLOR="#00ffff"/>
+<node TEXT="Dataiku" ID="ID_576094897" CREATED="1605218503607" MODIFIED="1605218514634"/>
+<node TEXT="Anaconda" ID="ID_1914912896" CREATED="1605218516747" MODIFIED="1605218520167"/>
+<node TEXT="MATLAB" ID="ID_207655884" CREATED="1605218520760" MODIFIED="1605218523612"/>
+<node TEXT="Databricks" ID="ID_632232981" CREATED="1605218524300" MODIFIED="1605218529801"/>
+</node>
+<node TEXT="Outils de visualisation" POSITION="left" ID="ID_957308389" CREATED="1605218434994" MODIFIED="1605218449068">
+<edge COLOR="#ff00ff"/>
+<node TEXT="Plotly" ID="ID_25215714" CREATED="1605218451115" MODIFIED="1605218455787"/>
+<node TEXT="Tableau" ID="ID_1552529570" CREATED="1605218457173" MODIFIED="1605218459858"/>
+<node TEXT="Qlik" ID="ID_1431365470" CREATED="1605218460391" MODIFIED="1605218468412"/>
+<node TEXT="AnyChart" ID="ID_1998725606" CREATED="1605218468931" MODIFIED="1605218473036"/>
+<node TEXT="Google Charts" ID="ID_558978402" CREATED="1605218478474" MODIFIED="1605218484282"/>
+<node TEXT="Sisense" ID="ID_967239526" CREATED="1605218485167" MODIFIED="1605218488708"/>
+</node>
+<node TEXT="domaines de comp&#xe9;tence" POSITION="left" ID="ID_606401869" CREATED="1605218677623" MODIFIED="1605219349081">
+<edge COLOR="#7c0000"/>
+<node TEXT="statistiques" ID="ID_1954767473" CREATED="1605218682564" MODIFIED="1605218688752"/>
+<node TEXT="visualisation de donn&#xe9;es" ID="ID_270126643" CREATED="1605218689628" MODIFIED="1605218704033"/>
+<node TEXT="traitement du signal" ID="ID_1122073677" CREATED="1605218704253" MODIFIED="1605218714361"/>
+<node TEXT="m&#xe9;thodes de r&#xe9;f&#xe9;rencement" ID="ID_1565102082" CREATED="1605218715174" MODIFIED="1605218724614"/>
+<node TEXT="machine learning" ID="ID_1487770014" CREATED="1605218729307" MODIFIED="1605218734041"/>
+<node TEXT="math&#xe9;matiques" ID="ID_889927345" CREATED="1605218839125" MODIFIED="1605218842733"/>
+</node>
+</node>
+</map>
