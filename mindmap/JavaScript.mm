22fdb7894e74a974c5cf352ed24c30c20eb178cc
duchenne <duchenne.sebastien@gmail.com>
2020-11-19 22:01:38 +0100

    19 nov 2020

---
+++ mindmap/JavaScript.mm
@@ -0,0 +1,99 @@
+<map version="freeplane 1.7.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="JavaScript" FOLDED="false" ID="ID_1650794818" CREATED="1605732090394" MODIFIED="1605732104892" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="5" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Front" POSITION="right" ID="ID_1246134524" CREATED="1604608102382" MODIFIED="1605732118396">
+<edge COLOR="#00ffff"/>
+<node TEXT="Angular" ID="ID_1154927454" CREATED="1604608109079" MODIFIED="1604608111509">
+<node TEXT="NgRx" ID="ID_582398419" CREATED="1604610697911" MODIFIED="1604610700514"/>
+<node TEXT="RxJS" ID="ID_173266102" CREATED="1605047027116" MODIFIED="1605047031798"/>
+</node>
+<node TEXT="React" ID="ID_148826217" CREATED="1604608112906" MODIFIED="1604608115481">
+<node TEXT="Redux" ID="ID_214909829" CREATED="1604610621785" MODIFIED="1604610624719"/>
+<node TEXT="Router" ID="ID_1577708053" CREATED="1604610750714" MODIFIED="1604610753769"/>
+<node TEXT="Next.js" ID="ID_294487221" CREATED="1604609264220" MODIFIED="1604609275381"/>
+</node>
+<node TEXT="VueJS" ID="ID_1883680487" CREATED="1604608116225" MODIFIED="1604608119550">
+<node TEXT="VueX" ID="ID_1302177021" CREATED="1605047058878" MODIFIED="1605047065222"/>
+<node TEXT="Nuxt.js" ID="ID_1264307579" CREATED="1605047500682" MODIFIED="1605047504934"/>
+</node>
+</node>
+<node TEXT="NPM" POSITION="right" ID="ID_79866014" CREATED="1604608845754" MODIFIED="1605732116329">
+<edge COLOR="#00ff00"/>
+</node>
+<node TEXT="Babel" POSITION="right" ID="ID_1697377962" CREATED="1604609260689" MODIFIED="1605732114921">
+<edge COLOR="#0000ff"/>
+</node>
+<node TEXT="NodeJS" POSITION="left" ID="ID_863073621" CREATED="1604608429477" MODIFIED="1605732117233">
+<edge COLOR="#ff00ff"/>
+<node TEXT="chai" ID="ID_654938272" CREATED="1604609871298" MODIFIED="1604609874077"/>
+<node TEXT="eslint" ID="ID_1811026043" CREATED="1604609888845" MODIFIED="1604609891991"/>
+<node TEXT="Enzyme" ID="ID_526984968" CREATED="1605047209905" MODIFIED="1605047212842"/>
+<node TEXT="Jest" ID="ID_1247801527" CREATED="1605047166859" MODIFIED="1605047169128"/>
+<node TEXT="express" ID="ID_196515707" CREATED="1604610204064" MODIFIED="1604610206316"/>
+<node TEXT="Puppeteer" ID="ID_221895244" CREATED="1605478162815" MODIFIED="1605478168506"/>
+</node>
+</node>
+</map>
---
+++ mindmap/Nodejs.mm
@@ -0,0 +1,88 @@
+<map version="freeplane 1.7.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Nodejs" FOLDED="false" ID="ID_510123161" CREATED="1605566479909" MODIFIED="1605566485720" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="7" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="axios - requ&#xea;te" POSITION="right" ID="ID_1951849898" CREATED="1605731388353" MODIFIED="1605732976835">
+<edge COLOR="#00007c"/>
+</node>
+<node TEXT="Puppeteer - scrap" POSITION="right" ID="ID_221895244" CREATED="1605478162815" MODIFIED="1605732983356">
+<edge COLOR="#7c0000"/>
+</node>
+<node TEXT="express - serveur" POSITION="right" ID="ID_196515707" CREATED="1604610204064" MODIFIED="1605732992920">
+<edge COLOR="#00ffff"/>
+</node>
+<node TEXT="eslint" POSITION="right" ID="ID_1811026043" CREATED="1604609888845" MODIFIED="1605732287682">
+<edge COLOR="#ff00ff"/>
+</node>
+<node TEXT="test" POSITION="left" ID="ID_1351697862" CREATED="1605731339707" MODIFIED="1605731445890">
+<edge COLOR="#00ff00"/>
+<node TEXT="Jest" ID="ID_1247801527" CREATED="1605047166859" MODIFIED="1605047169128"/>
+<node TEXT="Enzyme" ID="ID_526984968" CREATED="1605047209905" MODIFIED="1605047212842"/>
+<node TEXT="chai" ID="ID_654938272" CREATED="1604609871298" MODIFIED="1604609874077"/>
+<node TEXT="Protractor" ID="ID_522326608" CREATED="1605731426582" MODIFIED="1605731430172"/>
+<node TEXT="Mocha" ID="ID_917879920" CREATED="1605731536753" MODIFIED="1605731538726"/>
+</node>
+</node>
+</map>
