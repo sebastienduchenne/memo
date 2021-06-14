113d887fac4d4ab308678df78a5f50409c7cacb6
sebastienduchenne <sigmapegasus@gmail.com>
2021-06-14 23:42:45 +0200

    14 juin 2021

--- Dev/front web/Reactjs/Modules/Router.js
+++ Dev/front web/Reactjs/Modules/Router.js
@@ -1,7 +1,34 @@
-// -installer react-router : $ npm install react-router
+`
+-installer react-router : $ npm install react-router
+
+*** API
+
+-Hooks
+   useHistory     : accéder à l'historique
+   useLocation    : retourne la location
+   useParams      : retourne un objet représentant les paramètres d'une URL
+   useRouteMatch  : tenter de faire correspondre l'URL actuelle de la même manière qu'un <Route> le ferait
+<BrowserRouter>   : un <Router> qui utilise l'historique HTML5
+<HashRouter>      : un <Router> qui utilise le hash de l'URL
+<Link>            : déclarer un lien
+<NavLink>         : un <Link> qui contient un attribut Style
+<Prompt>
+<MemoryRouter>    : un <Router> qui garde l'historique de l'URL en mémoire
+<Redirect>        : rediriger vers une location
+<Route>           : déclarer une route
+<Router>          : définir un bout d'appli concerné par le routage
+<StaticRouter>    : un <Router> dont la location est statique
+<Switch>          : rend dans la 1ère route qui correspond à l'URL
+generatePath()    : créer un chemin pour une route
+history           : objet qui représente un historique de navigation
+location          : objet qui représente un "lieu"
+match             : objet qui contient des informations sur la correspondance entre une URL et une <Route path>
+matchPath
+withRouter        : HOC
 
 
-//----------------- main.js -----------------
+----------------- main.js -----------------
+`
 
 import React from 'react';
 import ReactDOM from 'react-dom';
